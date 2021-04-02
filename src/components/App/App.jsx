dbaa94201ce3bf08edd1ed1a3030dbcead593aaf
Johnathan <johnathanboyd5@gmail.com>
2021-04-02 17:11:18 -0500

    added axios GET to route

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
+
 function App() {
+  useEffect( ()=>{
+    getFeedback
+  }, [])
+
+  //function to GET
+  let getFeedback=()=>{
+    console.log( 'in getFeedback' );
+    axios.get( '/api/feedback' ).then( ( response ) =>{
+      // this is where dispatch would go, i believe.
+      console.log( 'back from getFeedback GET with:', response );
+    }).catch( ( err ) =>{
+      console.log( err );
+      alert( 'err from getMenu')
+    })
+  }
+  //function to POST
 
   return (
     <div className='App'>
